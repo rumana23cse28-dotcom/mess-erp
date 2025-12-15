cd4b3508ef1b04ad0acb6ebc608edb6549d826ac
Raushan Kumar <roushan23cse27@gemspolytechnic.edu.in>
2025-12-15 08:20:31 +0530

    Fix duplicate __main__ block for deployment

--- app.py
+++ app.py
@@ -647,6 +647,6 @@ def logout():
 
 
 # ================= RUN =================
-if __name__ == '__main__':
-   if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
